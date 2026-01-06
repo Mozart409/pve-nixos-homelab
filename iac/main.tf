c4932d53b42d2f490337ff964917b6c98b95bfaa
Amadeus Mader <amadeus@mozart409.com>
2026-01-06 12:01:07 +0100

    fix: changed iso download

--- iac/main.tf
+++ iac/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 # Set the variable value in *.tfvars file
 variable "endpoint" {
-  sensitive =false 
+  sensitive = false
 }
 variable "username" {
   sensitive = true
@@ -28,15 +28,16 @@ provider "proxmox" {
 }
 
 
-# NixOS VM Configuration
-resource "proxmox_virtual_environment_file" "nixos_iso" {
+# NixOS ISO Download
+resource "proxmox_virtual_environment_download_file" "nixos_iso" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "nixos"
+  node_name    = "pve-gigabyte"
 
-  source_file {
-    path = "latest-nixos-minimal-x86_64-linux.iso"
-  }
+  url = "https://channels.nixos.org/nixos-25.11/latest-nixos-minimal-x86_64-linux.iso"
+
+  file_name = "nixos-25.11-minimal-x86_64.iso"
+  overwrite = false
 }
 
 resource "proxmox_virtual_environment_vm" "nixos_vm" {
@@ -44,7 +45,7 @@ resource "proxmox_virtual_environment_vm" "nixos_vm" {
   description = "NixOS VM - Managed by Terraform"
   tags        = ["terraform", "nixos"]
 
-  node_name = "nixos"
+  node_name = "pve-gigabyte"
   vm_id     = 4322
 
   # Boot from ISO
@@ -74,7 +75,7 @@ resource "proxmox_virtual_environment_vm" "nixos_vm" {
 
   cdrom {
     enabled = true
-    file_id = proxmox_virtual_environment_file.nixos_iso.id
+    file_id = proxmox_virtual_environment_download_file.nixos_iso.id
   }
 
   operating_system {
@@ -90,7 +91,124 @@ resource "proxmox_virtual_environment_vm" "nixos_vm" {
 
   serial_device {}
 
+  # Start VM after creation for initial nixos-anywhere installation
+  started = true
+
   # Don't start on boot initially (manual installation required)
+  on_boot = false
+}
+
+# PostgreSQL Database VM
+resource "proxmox_virtual_environment_vm" "database_vm" {
+  name        = "database"
+  description = "PostgreSQL Database - Managed by Terraform"
+  tags        = ["terraform", "nixos", "database"]
+
+  node_name = "pve-gigabyte"
+  vm_id     = 4323
+
+  bios = "seabios"
+
+  keyboard_layout = "de"
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 4096
+    floating  = 4096
+  }
+
+  disk {
+    datastore_id = "zfs_pool"
+    size         = 64
+    interface    = "scsi0"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  cdrom {
+    enabled = true
+    file_id = proxmox_virtual_environment_download_file.nixos_iso.id
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  initialization {
+    user_account {
+      username = "amadeus"
+      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv1USrKf6yIjg8dZolm37xGysGfj18ol1KUKqsVuQHa amadeus@wotan"]
+    }
+  }
+
+  serial_device {}
+
+  # Start VM after creation for initial nixos-anywhere installation
+  started = true
+
+  on_boot = false
+}
+
+# Caddy Web Server VM
+resource "proxmox_virtual_environment_vm" "caddy_vm" {
+  name        = "caddy"
+  description = "Caddy Reverse Proxy - Managed by Terraform"
+  tags        = ["terraform", "nixos", "webserver"]
+
+  node_name = "pve-gigabyte"
+  vm_id     = 4324
+
+  bios = "seabios"
+
+  keyboard_layout = "de"
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 2048
+    floating  = 2048
+  }
+
+  disk {
+    datastore_id = "zfs_pool"
+    size         = 32
+    interface    = "scsi0"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  cdrom {
+    enabled = true
+    file_id = proxmox_virtual_environment_download_file.nixos_iso.id
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  initialization {
+    user_account {
+      username = "amadeus"
+      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv1USrKf6yIjg8dZolm37xGysGfj18ol1KUKqsVuQHa amadeus@wotan"]
+    }
+  }
+
+  serial_device {}
+
+  # Start VM after creation for initial nixos-anywhere installation
+  started = true
+
   on_boot = false
 }
 
