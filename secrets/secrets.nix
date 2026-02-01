fb391f4b39647da170c89ae9282310c76a17b585
Amadeus Mader <amadeus@mozart409.com>
2026-02-01 18:08:34 +0100

    feat(secrets): add unifi host to age encryption keys
    - Add SSH key for homelab-unifi host
    - Update host key comments for consistency
    - Re-encrypt tailscale auth key with new keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   amadeus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv1USrKf6yIjg8dZolm37xGysGfj18ol1KUKqsVuQHa amadeus@wotan";
-  hostDatabase = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOzKKGEVZy4w556Y3n1KQQrWVJUxU7XfHULii9W1qTr root@otel";
-  hostOtel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGz4mCD5XyFkwVaSzzWHhral8WqMGo01nKZM3gAX2vzP root@homelab-otel";
-  users = [amadeus hostDatabase hostOtel];
+  hostDatabase = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOzKKGEVZy4w556Y3n1KQQrWVJUxU7XfHULii9W1qTr amadeus@homelab-database";
+  hostOtel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGz4mCD5XyFkwVaSzzWHhral8WqMGo01nKZM3gAX2vzP amadeus@homelab-otel";
+  hostUnifi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG1dva0wW3yY7pu0bT2HafVcn08BZMjzTwEh3CGcdfb8 root@homelab-unifi";
+  users = [amadeus hostDatabase hostOtel hostUnifi];
 in {
   "tailscale-auth-key.age".publicKeys = users;
 }
