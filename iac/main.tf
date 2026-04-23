38c3a76fb17113412926e91b4573ffc6dcacafb0
Amadeus Mader <amadeus@mozart409.com>
2026-04-23 15:08:30 +0200

    feat: add fleet vm

--- iac/main.tf
+++ iac/main.tf
@@ -505,6 +505,73 @@ resource "proxmox_virtual_environment_vm" "hermes_vm" {
   on_boot = false
 }
 
+# Fleet (osquery management) VM
+resource "proxmox_virtual_environment_vm" "fleet_vm" {
+  name        = "fleet"
+  description = "Fleet osquery management server - Debian base for NixOS installation via nixos-anywhere"
+  tags        = ["terraform", "debian", "nixos-target", "security", "fleet"]
+
+  node_name = "pve-gigabyte"
+  vm_id     = 4338
+
+  bios = "seabios"
+
+  keyboard_layout = "de"
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 4096
+    floating  = 4096
+  }
+
+  disk {
+    datastore_id = "zfs_pool"
+    file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
+    interface    = "scsi0"
+    size         = 32
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  initialization {
+    datastore_id = "local-lvm"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.2.165/24"
+        gateway = "192.168.2.1"
+      }
+    }
+
+    user_account {
+      username = "amadeus"
+      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv1USrKf6yIjg8dZolm37xGysGfj18ol1KUKqsVuQHa amadeus@wotan"]
+    }
+  }
+
+  serial_device {}
+
+  # Enable QEMU Guest Agent
+  agent {
+    enabled = true
+    timeout = "60s"
+  }
+
+  started = true
+
+  on_boot = true
+}
+
 # Certificate Authority (step-ca) VM
 resource "proxmox_virtual_environment_vm" "ca_vm" {
   name        = "ca"
@@ -731,6 +798,7 @@ output "vm_ipv4_addresses" {
     mcp          = proxmox_virtual_environment_vm.mcp_vm.ipv4_addresses
     hermes       = proxmox_virtual_environment_vm.hermes_vm.ipv4_addresses
     ca           = proxmox_virtual_environment_vm.ca_vm.ipv4_addresses
+    fleet        = proxmox_virtual_environment_vm.fleet_vm.ipv4_addresses
     k3s_server_1 = proxmox_virtual_environment_vm.k3s_server_1_vm.ipv4_addresses
     k3s_agent_1  = proxmox_virtual_environment_vm.k3s_agent_1_vm.ipv4_addresses
   }
