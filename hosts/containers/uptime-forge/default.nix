08b8bec53f4811cc71c8c562fdc3254205126669
Amadeus Mader <amadeus@mozart409.com>
2026-04-27 12:10:28 +0200

    feat: add base_path

--- hosts/containers/uptime-forge/default.nix
+++ hosts/containers/uptime-forge/default.nix
@@ -91,7 +91,7 @@ in {
     };
 
     uptime-forge = {
-      image = "ghcr.io/mozart409/uptime-forge:v0.2.6";
+      image = "ghcr.io/mozart409/uptime-forge:v0.2.8";
       autoStart = true;
       ports = ["3000:3000"];
       volumes = [
