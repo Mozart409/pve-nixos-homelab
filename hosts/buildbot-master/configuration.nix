e61b866f9bc57d6eaa41bdcc0348b67ff7a9cd98
Amadeus Mader <amadeus@mozart409.com>
2026-05-12 10:23:20 +0200

    fix: change config

--- hosts/buildbot-master/configuration.nix
+++ hosts/buildbot-master/configuration.nix
@@ -3,7 +3,94 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  # Buildbot master configuration file
+  masterCfgFile = pkgs.writeText "master.cfg" ''
+    from buildbot.plugins import *
+    from buildbot.process.properties import Interpolate
+    import os
+
+    c = BuildmasterConfig = {}
+
+    # Worker configuration - read password at runtime
+    worker_password_file = "/run/agenix/buildbot-worker-password"
+    with open(worker_password_file) as f:
+        worker_password = f.read().strip()
+
+    c["workers"] = [
+        worker.Worker("worker-1", worker_password),
+    ]
+
+    c["protocols"] = {"pb": {"port": 9989}}
+
+    # Change sources - Forgejo webhook
+    c["change_source"] = []
+
+    # Schedulers
+    c["schedulers"] = [
+        schedulers.SingleBranchScheduler(
+            name="all",
+            change_filter=util.ChangeFilter(branch="main"),
+            treeStableTimer=60,
+            builderNames=["nix-build"],
+        ),
+        schedulers.ForceScheduler(
+            name="force",
+            builderNames=["nix-build"],
+        ),
+    ]
+
+    # Build factory for Nix builds
+    nix_factory = util.BuildFactory()
+    nix_factory.addStep(steps.Git(
+        repourl=util.Property("repository", default=""),
+        mode="incremental",
+        submodules=True,
+    ))
+    nix_factory.addStep(steps.ShellCommand(
+        name="nix flake check",
+        command=["nix", "flake", "check"],
+        haltOnFailure=True,
+    ))
+    nix_factory.addStep(steps.ShellCommand(
+        name="nix build",
+        command=["nix", "build"],
+        haltOnFailure=True,
+    ))
+
+    c["builders"] = [
+        util.BuilderConfig(
+            name="nix-build",
+            workernames=["worker-1"],
+            factory=nix_factory,
+        ),
+    ]
+
+    # Web interface
+    c["www"] = {
+        "port": 8010,
+        "plugins": {
+            "waterfall_view": {},
+            "console_view": {},
+            "grid_view": {},
+        },
+    }
+
+    # Database - PostgreSQL with secret from file
+    c["secretsProviders"] = [
+        secrets.SecretInAFile(dirname="/var/lib/buildbot/master/secrets"),
+    ]
+
+    c["db"] = {
+        "db_url": "postgresql+psycopg2://buildbot:%(secret:db_password)s@192.168.2.134/buildbot",
+    }
+
+    # Project identity
+    c["title"] = "Homelab CI"
+    c["titleURL"] = "https://homelab-buildbot-master.dropbear-butterfly.ts.net/"
+    c["buildbotURL"] = "https://homelab-buildbot-master.dropbear-butterfly.ts.net/"
+  '';
+in {
   imports = [
     ../../modules/common.nix
     ../../modules/disko-config.nix
@@ -33,93 +120,16 @@
     group = "buildbot";
   };
 
+  age.secrets.buildbot-db-password = {
+    file = ../../secrets/buildbot-db-password.age;
+    owner = "buildbot";
+    group = "buildbot";
+  };
+
   # Buildbot Master
   services.buildbot-master = {
     enable = true;
-    masterCfg = ''
-      from buildbot.plugins import *
-      from buildbot.process.properties import Interpolate
-      import os
-
-      c = BuildmasterConfig = {}
-
-      # Worker configuration
-      worker_password = open("${config.age.secrets.buildbot-worker-password.path}").read().strip()
-      c["workers"] = [
-          worker.Worker("worker-1", worker_password),
-      ]
-
-      c["protocols"] = {"pb": {"port": 9989}}
-
-      # Change sources - Forgejo webhook
-      c["change_source"] = []
-
-      # Schedulers
-      c["schedulers"] = [
-          schedulers.SingleBranchScheduler(
-              name="all",
-              change_filter=util.ChangeFilter(branch="main"),
-              treeStableTimer=60,
-              builderNames=["nix-build"],
-          ),
-          schedulers.ForceScheduler(
-              name="force",
-              builderNames=["nix-build"],
-          ),
-      ]
-
-      # Build factory for Nix builds
-      nix_factory = util.BuildFactory()
-      nix_factory.addStep(steps.Git(
-          repourl=util.Property("repository", default=""),
-          mode="incremental",
-          submodules=True,
-      ))
-      nix_factory.addStep(steps.ShellCommand(
-          name="nix flake check",
-          command=["nix", "flake", "check"],
-          haltOnFailure=True,
-      ))
-      nix_factory.addStep(steps.ShellCommand(
-          name="nix build",
-          command=["nix", "build"],
-          haltOnFailure=True,
-      ))
-
-      c["builders"] = [
-          util.BuilderConfig(
-              name="nix-build",
-              workernames=["worker-1"],
-              factory=nix_factory,
-          ),
-      ]
-
-      # Web interface
-      c["www"] = {
-          "port": 8010,
-          "plugins": {
-              "waterfall_view": {},
-              "console_view": {},
-              "grid_view": {},
-          },
-      }
-
-      # Database - PostgreSQL
-      c["db"] = {
-          "db_url": "postgresql+psycopg2://buildbot:%(secret:db_password)s@192.168.2.134/buildbot",
-      }
-
-      # Read database password
-      db_password = open("${config.age.secrets.buildbot-db-password.path}").read().strip()
-      c["secretsProviders"] = [
-          secrets.SecretInAFile(dirname="${config.services.buildbot-master.buildbotDir}/secrets"),
-      ]
-
-      # Project identity
-      c["title"] = "Homelab CI"
-      c["titleURL"] = "https://homelab-buildbot-master.dropbear-butterfly.ts.net/"
-      c["buildbotURL"] = "https://homelab-buildbot-master.dropbear-butterfly.ts.net/"
-    '';
+    masterCfg = masterCfgFile;
     buildbotDir = "/var/lib/buildbot/master";
     packages = with pkgs; [
       git
@@ -149,12 +159,6 @@
       cp ${config.age.secrets.buildbot-db-password.path} /var/lib/buildbot/master/secrets/db_password
       chmod 600 /var/lib/buildbot/master/secrets/db_password
     '';
-  };
-
-  age.secrets.buildbot-db-password = {
-    file = ../../secrets/buildbot-db-password.age;
-    owner = "buildbot";
-    group = "buildbot";
   };
 
   # Prometheus exporters
