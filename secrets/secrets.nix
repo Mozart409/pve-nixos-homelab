1979f149785acb94f958ddacc40c673649154a0a
Amadeus Mader <amadeus@mozart409.com>
2026-06-08 15:38:05 +0200

    feat(hermes): add deepseek key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -33,6 +33,7 @@ in {
   "terraform-state-db-password.age".publicKeys = [amadeus amadeusAge hostDatabase];
   "hermes-opencode-zen-key.age".publicKeys = [amadeus amadeusAge hostHermes];
   "hermes-api-server-key.age".publicKeys = [amadeus amadeusAge hostHermes];
+  "hermes-deepseek-key.age".publicKeys = [amadeus amadeusAge hostHermes];
   "k3s-server-token.age".publicKeys = [amadeus amadeusAge hostK3sServer1 hostK3sWorker1];
   "step-ca-password.age".publicKeys = [amadeus amadeusAge hostCa];
   "fleet-enroll-secret.age".publicKeys = users;
